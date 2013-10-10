3881e09e7051348aecbdfccd53f34ddc3d5e1c91
darren <darren.miao@e-graphics.com>
2013-10-10 15:46:15 +0800

    admin

--- application/controllers/manage/admin.php
+++ application/controllers/manage/admin.php
@@ -6,6 +6,8 @@
 class Admin extends CI_Controller{
 	function __construct(){
 		parent::__construct();
+		$this->load->helper(array('file','url','cookie'));
+		$this->load->library(array('session','parser','email','image_lib','phpmail','user_agent','pagination','calendar'));
 	}
 	function index(){
 		echo "Admin pannel";
